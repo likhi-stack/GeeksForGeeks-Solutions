5e1713198dc5a11c09c0b6cb23c1735dc5528467
likhi-stack <karetilikhita687@gmail.com>
2026-09-20 21:48:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count Sum Pairs in Sorted/count-sum-pairs-in-sorted.java
+++ Difficulty: Easy/Count Sum Pairs in Sorted/count-sum-pairs-in-sorted.java
@@ -1,42 +1,35 @@
-class Solution {
-    int countPairs(int arr[], int target) {
-        //  Code Here
+class Solution{
+    int countPairs(int[] arr,int target){
         int l=0;
         int r=arr.length-1;
         int count=0;
-        while(l<r)
-        {
-            int s=arr[l]+arr[r];
-            if(s<target)
-            {
-                l++;
-            }
-            else if(s>target)
-            {
-                r--;
-            }
-            else
-            {
-                if(arr[l]==arr[r])
-                {
+        while(l<r){
+            int sum=arr[l]+arr[r];
+            if(sum==target){
+                if(arr[l]==arr[r]){
                     int n=r-l+1;
                     count+=n*(n-1)/2;
                     break;
                 }
-                int left=arr[l];
-                int right=arr[r];
+                int x=arr[l];
+                int y=arr[r];
                 int lc=0;
                 int rc=0;
-                while(l<r&&arr[l]==left)
-                {
+                while(l<r&&arr[l]==x){
                     lc++;
                     l++;
                 }
-                while(l<=r&&arr[r]==right){
+                while(l<=r&&arr[r]==y){
                     rc++;
                     r--;
                 }
                 count+=lc*rc;
+            }
+            else if(sum<target){
+                l++;
+            }
+            else{
+                r--;
             }
         }
         return count;
